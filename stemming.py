6d3a14dd1f58550b814e796f05107e8321add86e
Chi Le <cdle@cpp.edu>
2023-12-02 23:24:43 -0800

    Adding text transforming functionalities

--- stemming.py
+++ stemming.py
@@ -1,2 +1,12 @@
-def stem(list_of_token):
-    pass
+# -------------------------------------------------------------------------
+# FILENAME: stemming.py
+# SPECIFICATION: Uses lemmatizer to apply stemming to a list of tokens, returning the stemmed terms
+# FOR: CS 4250- Group Project
+# -----------------------------------------------------------*/
+
+import nltk
+
+def stem(tokens):
+    lemma = nltk.wordnet.WordNetLemmatizer()
+    terms = [lemma.lemmatize(token) for token in tokens]
+    return terms
--- tokenizer.py
+++ tokenizer.py
@@ -1,2 +1,23 @@
-def tokenize(html):
-    pass
+# -------------------------------------------------------------------------
+# FILENAME: tokenizer.py
+# SPECIFICATION: Removes special characters, splits the document into terms and
+# converts them to lowercase.
+# FOR: CS 4250- Group Project
+# -----------------------------------------------------------*/
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+def tokenize(document):
+    vectorizer = CountVectorizer(
+        lowercase=True,
+        token_pattern=r'\b\w+\b',  # Use a regex pattern to extract words & remove punctuation
+        strip_accents='unicode',
+    )
+
+    # Fit and transform the document
+    vector = vectorizer.fit_transform([document])
+
+    # Get the feature names (terms)
+    terms = vectorizer.get_feature_names_out()
+
+    return terms
