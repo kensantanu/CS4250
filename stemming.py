6d13e183511788df2a794676f39be619d0c17377
Chi Le <cdle@cpp.edu>
2023-12-02 23:36:27 -0800

    Added another stemming option

--- stemming.py
+++ stemming.py
@@ -1,12 +1,18 @@
 # -------------------------------------------------------------------------
 # FILENAME: stemming.py
-# SPECIFICATION: Uses lemmatizer to apply stemming to a list of tokens, returning the stemmed terms
+# SPECIFICATION: Apply stemming to a list of tokens, returning the stemmed terms
 # FOR: CS 4250- Group Project
 # -----------------------------------------------------------*/
 
 import nltk
+from nltk.stem.snowball import SnowballStemmer
 
 def stem(tokens):
+    # Using lemmatizer
     lemma = nltk.wordnet.WordNetLemmatizer()
     terms = [lemma.lemmatize(token) for token in tokens]
+
+    # Using stemmer
+    # snowball = SnowballStemmer(language="english")
+    # terms = [snowball.stem(token) for token in tokens]
     return terms
