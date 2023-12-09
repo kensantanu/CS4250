c3a69fc02baf1a0652d1b8c129ec2e936dd036d1
nathanvpham <natepham16@gmail.com>
2023-12-09 09:58:44 -0800

    added tokenizer to query

--- ir_model.py
+++ ir_model.py
@@ -1,12 +1,21 @@
 import tokenizer
 import stopping
 import stemming
+import db_connection
 
 def search(userQuery):
+    db = db_connection.connectDataBase()
     pass
     # tokenize userQuery
     # remove common words
     # stem tokens
+    tokens = tokenizer.tokenize(userQuery)
+
+        # Apply stopping
+    stopped_tokens = stopping.remove_common_words(tokens)
+
+        # Apply stemming
+    stemmed_tokens = stemming.stem(stopped_tokens)
 
     # calculate query weights?
 
